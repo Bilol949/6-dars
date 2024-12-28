55e2a832db1412132393e363503bc10453001307
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2024-12-28 19:00:16 +0500

    bilol

---
+++ main.js
@@ -0,0 +1,42 @@
+// let person = {
+   
+  
+//     tel:"77 123 45 67",
+//     manzil:"toshkent sh"
+// }
+
+// // person.ism = prompt("ismingizni kiriting").toUpperCase()
+
+// let yosh = prompt("yoshingizni kiriting")
+// if(yosh >= 15){
+//     person.yosh = yosh
+
+// }else{
+//     alert("siz hali kichkina siz")
+// }
+
+
+
+
+
+
+
+// person.yoqtirganfan = "fizika"
+
+// console.log(person);
+
+let person = {
+    
+}
+person.name = prompt("Ismingizni kiriting") 
+
+
+person.Id = prompt("space Id yingizni kiriting")
+
+person.password = prompt("password dingizni kiriting kiriting") 
+
+
+
+console.log(person);
+
+
